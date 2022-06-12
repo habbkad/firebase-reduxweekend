89431c8af0b5483bc818cb4f592a9fa6cb72aa01
habbkad <habibkadiri52@gmail.com>
2022-06-12 10:53:47 +0000

    done with create

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+import { v4 as uuid } from "uuid";
+import app from "./Firebase/Config";
+import { getFirestore } from "firebase/firestore";
+import { collection, addDoc, doc, setDoc } from "firebase/firestore";
+import UsersList from "./UsersList";
 
 function App() {
+  const [name, setName] = useState("");
+  const [gen, setGen] = useState("");
+
+  const db = getFirestore(app);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const newPerson = {
+      name: name,
+      gen: gen,
+      id: uuid(),
+    };
+
+    //works without custom id
+    // try {
+    //   const docRef = await addDoc(collection(db, "users"), newPerson);
+
+    //   console.log("Document written with ID: ", docRef.id);
+    // } catch (e) {
+    //   console.error("Error adding document: ", e);
+    // }
+
+    //works with custom id
+
+    try {
+      await setDoc(doc(db, "Users", newPerson.id), newPerson);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          placeholder="Name"
+        />
+        <input
+          value={gen}
+          onChange={(e) => {
+            setGen(e.target.value);
+          }}
+          placeholder="gen"
+        />
+        <br />
+        <button>submit</button>
+      </form>
+
+      <br />
+
+      <UsersList />
     </div>
   );
 }
---
+++ src/Firebase/Config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAU0IF91ZpOZ1svtvwGedf3IkPsMmddaEs",
+  authDomain: "fir-reduxweekend.firebaseapp.com",
+  projectId: "fir-reduxweekend",
+  storageBucket: "fir-reduxweekend.appspot.com",
+  messagingSenderId: "590856281359",
+  appId: "1:590856281359:web:054d41de12dba3de42f27a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
